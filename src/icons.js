32034e42c2e30bf99c1e97c1a7928effb3cb4123
moocifur <davidkttran@gmail.com>
2024-12-29 22:34:28 -0800

    main shell works minmally

--- src/icons.js
+++ src/icons.js
@@ -1,10 +1,26 @@
 // icons.js
+// export const glassIcons = {
+//     rocks: "./assets/rocks.jpg",
+//     martini: "./assets/martini.jpeg",
+//     coupe: "./assets/coupe.jpeg",
+//     collins: "./assets/collins.jpeg",
+//     flute: "./assets/flute.jpeg",
+//     default: "./assets/default.jpeg"
+// };
+
+import rocks from "./assets/rocks.jpg";
+import martini from "./assets/martini.jpeg";
+import coupe from "./assets/coupe.jpeg";
+import collins from "./assets/collins.jpeg";
+import flute from "./assets/flute.jpeg";
+import defaultIcon from "./assets/default.jpeg";
+
 export const glassIcons = {
-    rocks: "./assets/rocks.jpg",
-    martini: "./assets/martini.jpeg",
-    coupe: "./assets/coupe.jpeg",
-    collins: "./assets/collins.jpeg",
-    flute: "./assets/flute.jpeg",
-    default: "./assets/default.jpeg"
+    rocks,
+    martini,
+    coupe,
+    collins,
+    flute,
+    default: defaultIcon
 };
 
